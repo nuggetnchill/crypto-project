00b058f190ef670dd0fbc57630a7d03cdacb30f8
Justin Nguyen <nuggetnchill@gmail.com>
2020-11-22 10:58:52 -0600

    timeframe options are ready to use

--- src/API.js
+++ src/API.js
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react';
 import {
   Area,
   AreaChart,
-  LineChart,
-  Line,
   CartesianGrid,
   XAxis,
   YAxis,
@@ -17,6 +15,7 @@ const yesterday = new Date(new Date() - DAY_IN_MS).toISOString();
 const aWeekAgo = new Date(new Date() - 7 * DAY_IN_MS).toISOString();
 const aMonthAgo = new Date(new Date() - 31 * DAY_IN_MS).toISOString();
 const aYearAgo = new Date(new Date() - 365 * DAY_IN_MS).toISOString();
+const firstDay = new Date(new Date().getFullYear(), 0, 1).toISOString();
 
 // Create our number formatter // Handling monetary currency(USD)
 const usdFormat = new Intl.NumberFormat('en-US', {
@@ -31,7 +30,7 @@ const usdFormat = new Intl.NumberFormat('en-US', {
 // const NOMICS_API_URL = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.REACT_APP_KEY}&ids=BTC,ETH,XRP&interval=1d,30d&per-page=100&page=1`;
 
 const API = () => {
-  const [timeframe, setTimeFrame] = useState('1D');
+  const [timeframe, setTimeframe] = useState('1D');
   const [searchField, setSearchField] = useState('ETH');
   const [rate, setRate] = useState([]);
   const [timestamp, setTimestamp] = useState([]);
@@ -46,27 +45,53 @@ const API = () => {
 
     // Splitting data into 2 arrays
     // https://medium.com/javascript-in-plain-english/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
-    // ^ this link was a good read about using spread operator and wrapper fn for problem bellow
-    data.forEach((el) => {
+    // ^ this link was a good read about using spread operator and wrapper fn for problem below
+    await data.forEach((el) => {
       setTimestamp((timestamp) => [...timestamp, el.timestamp]);
       setRate((rate) => [...rate, el.rate]);
+      console.log('setTimestamp and setRate');
     });
-  };
+    await console.log('fetch!');
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    // await addChartData();
+    await console.log('chartData added from fetch');
+  };
 
   // Combining the arrays that I splitted earlier after modifications
   let chartData = [];
-  rate.forEach((each, i) => {
-    chartData.push({
-      // timestamp: new Date(timestamp[i]).toLocaleString(),
-      timestamp: timestamp[i],
-      rate: (each * 1).toFixed(3),
+  // const addChartData = () => {
+  if (chartData.length == 0 && rate.length > 0) {
+    rate.forEach((each, i) => {
+      chartData.push({
+        // timestamp: new Date(timestamp[i]).toLocaleString(),
+        timestamp: timestamp[i],
+        rate: (each * 1).toFixed(3),
+      });
     });
-  });
+  }
 
+  //   console.log('from chartData function: ' + chartData);
+  // };
+
+  console.log(chartData);
+
+  const initialLoad = async () => {
+    await fetchData();
+    await console.log('initial fetch!');
+  };
+
+  useEffect(() => {
+    initialLoad();
+  }, []);
+
+  // useEffect(() => {
+  //   fetchData();
+  //   console.log('initial fetch!');
+  //   addChartData();
+  //   console.log('initial addChartData!');
+  // }, []);
+
+  // USER INPUT, BUTTONS Fn
   const handleInput = (event) => {
     setSearchField(event.target.value.toUpperCase());
   };
@@ -87,6 +112,18 @@ const API = () => {
     }
   };
 
+  // HANDLING WHEN timeframe CHANGES
+
+  const timeframeButton = async (event) => {
+    chartData = [];
+    setTimeframe(event.target.innerText);
+    await setRate([]);
+    await setTimestamp([]);
+    await setStartDate(event.target.id);
+    await fetchData();
+  };
+
+  // RENDER HERE:
   return (
     <div className='container'>
       <input
@@ -99,6 +136,7 @@ const API = () => {
       <button onClick={handleSubmit}>Search</button>
 
       <h2>Currency: {searchField}</h2>
+      <h3>({timeframe})</h3>
 
       {chartData.length > 0 && (
         <h1>
@@ -108,12 +146,22 @@ const API = () => {
 
       <div className='timeframe'>
         <ul className='timeframe-options'>
-          <li>1D</li>
-          <li>1W</li>
-          <li>1M</li>
-          <li>1Y</li>
-          <li>YTD</li>
-          <li>All</li>
+          <li onClick={timeframeButton} id={yesterday}>
+            1D
+          </li>
+          <li onClick={timeframeButton} id={aWeekAgo}>
+            1W
+          </li>
+          <li onClick={timeframeButton} id={aMonthAgo}>
+            1M
+          </li>
+          <li onClick={timeframeButton} id={aYearAgo}>
+            1Y
+          </li>
+          <li onClick={timeframeButton} id={firstDay}>
+            YTD
+          </li>
+          <li onClick={timeframeButton}>All</li>
         </ul>
       </div>
 
@@ -222,7 +270,6 @@ const API = () => {
             dataKey='rate'
             stroke='#00E08E'
             fill='#82ca9e50'
-            fill='#1d442c50 '
           />
           <YAxis hide={true} domain={['auto', 'auto']} />
         </AreaChart>
